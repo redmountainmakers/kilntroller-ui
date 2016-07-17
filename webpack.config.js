e6c2c203bc99fc568d460344580a5aa3a98108fa
James Nylen <jnylen@gmail.com>
2016-07-17 04:28:52 -0500

    Exclude moment.js locales (build: 572 kB -> 426 kB)

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,7 @@ const config = {
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify(env),
         }),
+        new webpack.IgnorePlugin(/\.\/locale$/, /moment$/),
     ],
 };
 
