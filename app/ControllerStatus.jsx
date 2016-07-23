6bf6aa66f356c9d79b386fada9564c8e98b3d4e3
James Nylen <jnylen@gmail.com>
2016-07-23 16:48:35 -0500

    Add icon and styling to ControllerStatus

--- app/ControllerStatus.jsx
+++ app/ControllerStatus.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import Gridicon from '../vendor/gridicon';
 import {
     isControllerConnected,
 } from './selectors/controller';
@@ -10,13 +11,17 @@ import './ControllerStatus.scss';
 function ControllerStatus({ requesting, isConnected, error }) {
     if (requesting) {
         return (
-            <div>Loading controller status...</div>
+            <div className="ControllerStatus loading">
+                <Gridicon icon="plugins" size={ 24 }/>
+                Connecting to controller...
+            </div>
         );
     }
 
     if (isConnected) {
         return (
             <div className="ControllerStatus success">
+                <Gridicon icon="plugins" size={ 24 }/>
                 Connected to controller
             </div>
         );
@@ -25,11 +30,12 @@ function ControllerStatus({ requesting, isConnected, error }) {
     if (error) {
         return (
             <div className="ControllerStatus error">
-                <div>
-                    Error connecting to controller:
+                <Gridicon icon="plugins" size={ 24 }/>
+                <div className="line">
+                    Read-only due to error:&nbsp;
                     { error }
                 </div>
-                <div>
+                <div className="line">
                     Be sure you are connected to the RMM wifi network and the
                     kiln controller is on.
                 </div>
