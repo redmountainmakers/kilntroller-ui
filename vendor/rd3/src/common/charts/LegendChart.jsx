ca8a6c29bcf0289d006f273f85443861069897ca
James Nylen <jnylen@gmail.com>
2016-07-24 23:49:02 -0500

    Fix the legend obscuring part of the chart

--- vendor/rd3/src/common/charts/LegendChart.jsx
+++ vendor/rd3/src/common/charts/LegendChart.jsx
@@ -99,11 +99,11 @@ module.exports = React.createClass({
         style={{ width: props.width, height: props.height }}
       >
         {this._renderTitle()}
-        <div style={{ display: 'table', width: '100%', height: '100%' }}>
-          <div style={{ display: 'table-cell' }}>
+        <div className="container">
+          <div className="chart">
             {this._renderChart()}
           </div>
-          <div style={{ display: 'table-cell', width: props.sideOffset, verticalAlign: 'top' }}>
+          <div className="legend" style={{ width: props.sideOffset }}>
             {this._renderLegend()}
           </div>
         </div>
